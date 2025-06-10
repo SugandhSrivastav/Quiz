737c258cb1a4f8a902c9eba7bd4bea1b1abd47ba
Sugandh Srivastav <srivastavsugandh@gmail.com>
2025-06-10 23:39:24 +0530

    feat: added Eureka Client for service registration on Eureka Server

--- src/main/java/com/quiz/QuizApplication.java
+++ src/main/java/com/quiz/QuizApplication.java
@@ -2,10 +2,11 @@ package com.quiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class QuizApplication {
 
 	public static void main(String[] args) {
